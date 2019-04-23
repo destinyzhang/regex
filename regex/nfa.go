a5efed2f87e2af5046c18220b2b2e758e3b2f19d
Destiny <569964924@qq.com>
2019-04-23 17:06:15 +0800

    init

---
+++ regex/nfa.go
@@ -0,0 +1,6 @@
+package regex
+
+//NFATree nfa结构
+type NFATree struct {
+	Init *State //初始状态
+}
---
+++ regex/state.go
@@ -0,0 +1,20 @@
+package regex
+
+//TransLink 状态转换
+type TransLink struct {
+	EpsilonLink bool   //是否空连转换
+	Token       rune   //对应转换token
+	EndState    *State //转换目标状态
+}
+
+//State 状态
+type State struct {
+	ID         int          //状态id
+	IsAccept   bool         //是否可接受状态
+	TransLinks []*TransLink //状态转换
+}
+
+//FindEpsilonSet 返回闭包集合
+func (state *State) FindEpsilonSet() []*State {
+	return nil
+}
